bce1e260bcc43250cf52ebf919eb162d6c5af4f3
RatneshNamdev <ratnesh.mygithub@gmail.com>
2022-03-04 18:06:53 +0530

    WH-0402 : Initial comment...

---
+++ src/main/java/com/lise/RestAssured/Demo.java
@@ -0,0 +1,28 @@
+package com.lise.RestAssured;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.testng.annotations.*;
+
+public class Demo
+{
+    @BeforeSuite
+    public void test(){
+        System.out.println("This will execute before every method..");
+    }
+
+    @Test()
+    @Owner("Ratnesh")
+    @Severity(SeverityLevel.NORMAL)
+    @Description("This is demo.. of report file...")
+    public void setUp(){
+        System.out.println("This is Demo one..");
+    }
+
+    @AfterSuite
+    public void test1(){
+        System.out.println("This will execute after every method...");
+    }
+}
